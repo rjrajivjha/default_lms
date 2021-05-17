c51d1db6486a6893b01b8ac1581b3fab75ebe37e
rjrajivjha <rajivjha1996@gmail.com>
2021-05-17 10:08:41 +0530

    Update views

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import viewsets, permissions, views, filters
-from rest_framework.exceptions import APIException
+
 from .models import Book, IssueLog, IssueRequest
 from .serializers import BookSerializer, BookIssueRequestSerializer, BookIssueLogSerializer
+from rest_framework import viewsets, permissions, views, filters
+from rest_framework.exceptions import APIException
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -15,7 +15,7 @@ class BookViewSet(viewsets.ModelViewSet):
 
 
 def check_book_not_issued_to_user(borrower, book):
-    qs = IssueLog.objects.filter(book=book,borrower=borrower,deposit_date=None)
+    qs = IssueLog.objects.filter(book=book, borrower=borrower, deposit_date=None)
     if qs:
         return False
     return True
@@ -32,63 +32,59 @@ class IssueRequestViewSet(viewsets.ModelViewSet):
         try:
             book = Book.objects.get(pk=self.request.data['book'])
         except ObjectDoesNotExist:
-            raise APIException("Requested Book Does Not Exist")
+            raise APIException("Requested Book Does Not Exist.")
 
         try:
             issue_request = IssueRequest.objects.get(
-                book = self.request.data['book'], requester=self.request.data['requester'],
-                request_status=IssueRequest.RequestStatus.Requested
-            )
+                book=self.request.data['book'], requester=self.request.data['requester'],
+                request_status=IssueRequest.RequestStatus.Requested)
         except ObjectDoesNotExist:
             issue_request = None
 
         if issue_request:
-            raise APIException("Book has already been requested")
+            raise APIException("Book has already been requested.")
 
         if book.available > 0 and book.quantity > 0 and\
                 check_book_not_issued_to_user(self.request.data['requester'], self.request.data['book']):
             serializer.save()
         else:
-            raise APIException("There was a problem while Requesting to issue this book. \n"
+            raise APIException("There was a problem while Requesting to Issue this book. \n "
                                "Possible Reasons : "
-                               "1. Either Book is not available"
-                               "2. Book is already issued on your name")
+                               "1. Either Book is not available. "
+                               "2. Book is already issued to user.")
 
 
-def check_book_exist(book_id):
-    try:
-        book = Book.objects.get(pk=book_id)
-    except ObjectDoesNotExist:
-        raise APIException("Requested Book Does Not Exist")
-    return book
-
-
-class BookIssueLogViewSet(viewsets.ViewSet):
+class BookIssueLogViewSet(viewsets.ModelViewSet):
+    """
+    Deleting a Issue Log is not allowed.
+    """
     queryset = IssueLog.objects.all()
     serializer_class = BookIssueLogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, permissions.IsAdminUser]
-    search_fields = ['book__title', 'borrower__email', 'due_date', 'issued_date', 'deposit_date']
+    search_fields = ['book__title', 'borrower__email', 'issued_date', 'deposit_date']
     filter_backends = (filters.SearchFilter,)
-    http_method_names = ['get', 'post', 'put', 'patch', 'head']
+    http_method_names = ['get', 'post', 'head', 'put', 'patch']
 
     def perform_create(self, serializer):
-        book = check_book_exist(self.request.data['book'])
+        try:
+            book = Book.objects.get(pk=self.request.data['book'])
+        except ObjectDoesNotExist:
+            raise APIException("Requested Book Does Not Exist.")
         try:
             issue_request = IssueRequest.objects.get(
-                book=self.request.data['book'], requester=self.request.data['borrower']
-            )
+                book=self.request.data['book'], requester=self.request.data['borrower'])
         except ObjectDoesNotExist:
             issue_request = None
-        if book.available > 0 and book.quantity > 0 and check_book_not_issued_to_user(
-                self.request.data['borrower'], self.request.data['book']):
+        if book.available > 0 and book.quantity > 0 and\
+                check_book_not_issued_to_user(self.request.data['borrower'], self.request.data['book']):
             book.available -= 1
             serializer.save()
             book.save()
-            if issue_request and issue_request.request_status=='RQ':
-                issue_request.request_status= IssueRequest.RequestStatus.Issued
+            if issue_request and issue_request.request_status == 'RQ':
+                issue_request.request_status = IssueRequest.RequestStatus.Issued
                 issue_request.save()
         else:
-            raise APIException("There was a problem while Issuing this book to user. \n"
+            raise APIException("There was a problem while Issuing this book. \n "
                                "Possible Reasons : "
-                               "1. Either Book is not available"
+                               "1. Either Book is not available. "
                                "2. Book is already issued to user.")
