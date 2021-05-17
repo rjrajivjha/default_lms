25fe36a6a19b009e71cf7589e57af5622db29aa1
rjrajivjha <rajivjha1996@gmail.com>
2021-05-17 10:09:35 +0530

    Update serializer

--- core/serializers.py
+++ core/serializers.py
@@ -2,23 +2,22 @@ from rest_framework import serializers
 from .models import Book, IssueLog, IssueRequest
 
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'isbn', 'title', 'author', 'quantity', 'available']
         read_only_fields = ('id',)
 
 
-class BookIssueRequestSerializer(serializers.Serializer):
-    class Meta:
-        model = IssueRequest
-        fields = ['id', 'requested', 'book', 'request_status', 'request_date']
-        read_only_fields = ('id',)
-
-
-class BookIssueLogSerializer(serializers.Serializer):
+class BookIssueLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = IssueLog
-        fields = ['id', 'borrower', 'book', 'issued_date', 'due_date', 'deposit_date', 'penalty']
+        fields = ['id', 'book', 'borrower', 'issued_date', 'due_date', 'deposit_date', 'penalty']
         read_only_fields = ('id',)
 
+
+class BookIssueRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IssueRequest
+        fields = ['requestor', 'book', 'request_status', 'request_date']
+        read_only_fields = ('id',)
